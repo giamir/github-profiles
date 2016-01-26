c9a1d1792fc3950fd49f164034284229141ae07c
giamir <giamir.buoncristiani@gmail.com>
2016-01-26 20:12:04 +0000

    set up protractor

--- test/mocks/searchFactoryMock.js
+++ test/mocks/searchFactoryMock.js
@@ -1,11 +1,10 @@
 angular.module('searchFactoryMock', ['GitUserSearch'])
   .config(function($provide) {
     $provide.factory('Search', function($q) {
+      var response = { data: { items: 'cat' } };
       return {
         query: function(searchTerm) {
-          return $q.when({
-            data: { items: 'cat' }
-          });
+          return $q.when(response);
         }
       };
     });
