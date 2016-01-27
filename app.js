f4bfd10ac8da325971f0e806149cdcbc47e58aa3
giamir <giamir.buoncristiani@gmail.com>
2016-01-27 22:31:09 +0000

    deploying to heroku 4

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-// We require express and set it to a usable variable
 var express = require('express');
 var logger = require('morgan');
-// We call express and set it equal to a usable variable
 var app = express();
+
+app.set('port', (process.env.PORT || 8080));
 
 app.use(logger('dev'));
 app.use(express.static(__dirname));
@@ -11,4 +11,6 @@ app.get('/', function(req, res) {
     res.render('index.html');
 });
 
-app.listen(8080);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
